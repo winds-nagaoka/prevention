71366278734ccd967460b1e5f7300355532da717
ryo4004 <kato@ryo4004.net>
2020-09-06 17:32:12 +0900

    体温報告フォームを作成

--- app.js
+++ app.js
@@ -8,3 +8,28 @@ app.listen(3009)
 // クライアントアプリを返す
 const client = './client/build'
 app.use('/', express.static(client))
+
+// データベース
+const path = require('path')
+const NeDB = require('nedb')
+
+const database = new NeDB({
+  filename: path.join(__dirname, 'server/database/database.db'),
+  autoload: true,
+  timestampData: true
+})
+
+// ライブラリの読み込み
+const lib = require('./server/lib')
+
+app.post('/post', (req, res) => {
+  const { name, temperature } = req.body
+  console.log('[' + lib.showTime() + '] post:', name, temperature)
+  const reg = { name, temperature }
+  database.insert(reg, (error) => {
+    if (error) {
+      console.log('Database Error')
+    }
+    return res.json({status: true})
+  })
+})
--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,30 +1,60 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import request from 'superagent'
 import Logo from '../Component/Logo/Logo'
 
 import './Home.css'
 
 const Home = () => {
+  const [mode, setMode] = useState(0)
   const [temperature, setTemperature] = useState("")
-  const [name, setName] = useName("")
+  const [name, setName] = useState("")
+
+  const sendPost = (e) => {
+    e.preventDefault()
+    request.post('/post')
+      .type('form')
+      .send({ name, temperature })
+      .end((err, res) => {
+        if (res.body.status) {
+          setMode(1)
+        }
+      })
+  }
+
+  const back = (e) => {
+    e.preventDefault()
+    setMode(0)
+  }
+
   return (
     <div className='home'>
       <div className='logo'>
         <Logo />
       </div>
       <div className='layout'>
-        <div className='title'>
-          <h2>Survey</h2>
-          <h1>体温申請</h1>
-        </div>
-        <p>リリックホール入り口で体温測定ができます</p>
-        <p>記録は3週間を目安に削除されます</p>
-        <div>
-          <label>本日の体温</label>
-          <input onChange={(e) => setTemperature(e.target.value)} value={temperature} />
-          <label>氏名</label>
-          <input onChange={(e) => setName(e.target.value)} value={name} />
-        </div>
+        {mode === 0 && (
+          <>
+            <p>リリックホールの楽屋入口にて体温測定ができます</p>
+            <div className="form">
+              <label>本日の体温</label>
+              <input onChange={(e) => setTemperature(e.target.value)} value={temperature} type="number" />
+              <label>氏名</label>
+              <input onChange={(e) => setName(e.target.value)} value={name} />
+            </div>
+            <div className="button">
+              <button onClick={(e) => sendPost(e)}>送信</button>
+              <p>記録は3週間を目安に削除します</p>
+            </div>
+          </>
+        )}
+        {mode === 1 && (
+          <>
+            <p>ご協力ありがとうございました</p>
+            <div className="button">
+              <button onClick={(e) => back(e)}>戻る</button>
+            </div>            
+          </>
+        )}
       </div>
       <footer>
         <div>
